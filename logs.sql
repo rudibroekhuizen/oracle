2de016fc655e838465919e99ca8abcf81693a408
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2015-01-13 14:07:52 +0100

    Create logs.sql

---
+++ logs.sql
@@ -0,0 +1,20 @@
+set echo off
+set verify off
+set feedback off
+set head off
+set wrap off
+
+-- show complete rows
+set linesize 32767
+
+-- show all rows without printing colomn names again:
+set pagesize 0
+
+column col01 newline
+column col02 newline
+column col03 newline
+
+select 'originating_timestamp: '||to_char(originating_timestamp,'DD-MM-YYYY HH24:MI:SS') col01,
+       'message_level:         '||message_level col02,
+       'message_text:          '||message_text col03
+from x$dbgalertext where where originating_timestamp > sysdate-1/24;
