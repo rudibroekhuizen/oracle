bc2b45b43bd6a50e335902dc2d3c67823584d10c
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2015-01-13 14:11:19 +0100

    Update logs.sql

--- logs.sql
+++ logs.sql
@@ -1,4 +1,5 @@
 -- this will hopefully provide input for logstash
+-- sqlplus -s / as sysdba @logs.sql > log.txt
 
 set echo off
 set verify off
@@ -20,3 +21,4 @@ select 'originating_timestamp: '||to_char(originating_timestamp,'DD-MM-YYYY HH24
        'message_level:         '||message_level col02,
        'message_text:          '||message_text col03
 from x$dbgalertext where where originating_timestamp > sysdate-1/24;
+exit
