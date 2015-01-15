03f83e1665677b2328c268d4618f415a6931d974
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2015-01-15 11:43:46 +0100

    Update logs-02.sql

--- logs-02.sql
+++ logs-02.sql
@@ -1,3 +1,5 @@
+Check this: https://github.com/rudibroekhuizen/puppet-role_logstash/blob/master/files/logstash-oraclelog-02.conf
+
 select 'message_level: '||message_level, 'originating_timestamp: '||to_char(originating_timestamp,'DD-MM-YYYY HH24:MI:SS') from x$dbgalertext where originating_timestamp > sysdate-8/24;
 
 select 'message_level='||message_level, 'originating_timestamp='||to_char(originating_timestamp,'DD-MM-YYYY HH24:MI:SS') from x$dbgalertext where originating_timestamp > sysdate-8/24;
